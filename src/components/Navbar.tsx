91eb4f8a192673af6d0cee5912290b20c4fc4f81
Burak Bilgili <burak.bilgili@icloud.com>
2024-12-06 17:00:10 +0300

    add nav items some functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import Image from 'next/image';
 import Link from 'next/link';
+import Image from 'next/image';
 
 import { BsFilePdf } from 'react-icons/bs';
+
 import {
   FileIcon,
   GlobeIcon,
@@ -41,7 +42,64 @@ import {
 
 import DocumentInput from './DocumentInput';
 
+import { useEditorStore } from '@/store/useEditorStore';
+
 const Navbar = () => {
+  const { editor } = useEditorStore();
+
+  const insertTable = ({ rows, cols }: { rows: number; cols: number }) => {
+    editor
+      ?.chain()
+      .focus()
+      .insertTable({ rows, cols, withHeaderRow: false })
+      .run();
+  };
+
+  const onDownload = (blob: Blob, filename: string) => {
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+
+    a.href = url;
+    a.download = filename;
+    a.click();
+  };
+
+  const onSaveAsJSON = () => {
+    if (!editor) return;
+
+    const content = editor.getJSON();
+
+    const blob = new Blob([JSON.stringify(content)], {
+      type: 'application/json',
+    });
+
+    onDownload(blob, 'doc.json');
+  };
+
+  const onSaveAsHTML = () => {
+    if (!editor) return;
+
+    const content = editor.getHTML();
+
+    const blob = new Blob([content], {
+      type: 'text/html',
+    });
+
+    onDownload(blob, 'doc.html');
+  };
+
+  const onSaveAsText = () => {
+    if (!editor) return;
+
+    const content = editor.getText();
+
+    const blob = new Blob([content], {
+      type: 'text/plain',
+    });
+
+    onDownload(blob, 'doc.txt');
+  };
+
   return (
     <nav className='flex items-center justify-between'>
       <div className='flex gap-2 items-center'>
@@ -71,19 +129,27 @@ const Navbar = () => {
                     <MenubarSubContent
                       className='bg-white rounded'
                       alignOffset={-5}>
-                      <MenubarItem className='hover:bg-neutral-100 py-1'>
+                      <MenubarItem
+                        onClick={onSaveAsJSON}
+                        className='hover:bg-neutral-100 py-1'>
                         <FileJsonIcon className='size-4 me-2' />
                         JSON
                       </MenubarItem>
-                      <MenubarItem className='hover:bg-neutral-100 py-1'>
+                      <MenubarItem
+                        onClick={onSaveAsHTML}
+                        className='hover:bg-neutral-100 py-1'>
                         <GlobeIcon className='size-4 me-2' />
                         HTML
                       </MenubarItem>
-                      <MenubarItem className='hover:bg-neutral-100 py-1'>
+                      <MenubarItem
+                        onClick={() => window.print()}
+                        className='hover:bg-neutral-100 py-1'>
                         <BsFilePdf className='size-4 me-2' />
                         PDF
                       </MenubarItem>
-                      <MenubarItem className='hover:bg-neutral-100 py-1'>
+                      <MenubarItem
+                        onClick={onSaveAsText}
+                        className='hover:bg-neutral-100 py-1'>
                         <FileTextIcon className='size-4 me-2' />
                         Text
                       </MenubarItem>
@@ -122,14 +188,18 @@ const Navbar = () => {
                   align='start'
                   alignOffset={5}
                   className='rounded bg-white cursor-pointer'>
-                  <MenubarItem className='rounded hover:bg-neutral-100 py-1 min-w-44'>
+                  <MenubarItem
+                    onClick={() => editor?.chain().focus().undo().run()}
+                    className='rounded hover:bg-neutral-100 py-1 min-w-44'>
                     <Undo2Icon className='size-4 me-2' />
                     Undo
                     <MenubarShortcut className='ms-auto text-xs tracking-widest text-muted-foreground'>
                       ⌘Z
                     </MenubarShortcut>
                   </MenubarItem>
-                  <MenubarItem className='rounded hover:bg-neutral-100 py-1 min-w-44'>
+                  <MenubarItem
+                    onClick={() => editor?.chain().focus().redo().run()}
+                    className='rounded hover:bg-neutral-100 py-1 min-w-44'>
                     <Redo2Icon className='size-4 me-2' />
                     Redo
                     <MenubarShortcut className='ms-auto text-xs tracking-widest text-muted-foreground'>
@@ -157,16 +227,24 @@ const Navbar = () => {
                     <MenubarSubContent
                       className='bg-white rounded'
                       alignOffset={-5}>
-                      <MenubarItem className='hover:bg-neutral-100 py-1'>
+                      <MenubarItem
+                        onClick={() => insertTable({ cols: 2, rows: 2 })}
+                        className='hover:bg-neutral-100 py-1'>
                         <Grid2X2Icon className='size-4 me-2' />2 x 2
                       </MenubarItem>
-                      <MenubarItem className='hover:bg-neutral-100 py-1'>
+                      <MenubarItem
+                        onClick={() => insertTable({ cols: 3, rows: 3 })}
+                        className='hover:bg-neutral-100 py-1'>
                         <Grid2X2Icon className='size-4 me-2' />3 x 3
                       </MenubarItem>
-                      <MenubarItem className='hover:bg-neutral-100 py-1'>
+                      <MenubarItem
+                        onClick={() => insertTable({ cols: 4, rows: 4 })}
+                        className='hover:bg-neutral-100 py-1'>
                         <Grid2X2Icon className='size-4 me-2' />4 x 4
                       </MenubarItem>
-                      <MenubarItem className='hover:bg-neutral-100 py-1'>
+                      <MenubarItem
+                        onClick={() => insertTable({ cols: 5, rows: 5 })}
+                        className='hover:bg-neutral-100 py-1'>
                         <Grid2X2Icon className='size-4 me-2' />5 x 5
                       </MenubarItem>
                     </MenubarSubContent>
@@ -192,28 +270,44 @@ const Navbar = () => {
                     <MenubarSubContent
                       className='bg-white rounded'
                       alignOffset={-5}>
-                      <MenubarItem className='hover:bg-neutral-100 py-1 min-w-44'>
+                      <MenubarItem
+                        onClick={() =>
+                          editor?.chain().focus().toggleBold().run()
+                        }
+                        className='hover:bg-neutral-100 py-1 min-w-44'>
                         <BoldIcon className='size-4 me-2' />
                         Bold
                         <MenubarShortcut className='ms-auto text-xs tracking-widest text-muted-foreground'>
                           ⌘B
                         </MenubarShortcut>
                       </MenubarItem>
-                      <MenubarItem className='hover:bg-neutral-100 py-1'>
+                      <MenubarItem
+                        onClick={() =>
+                          editor?.chain().focus().toggleItalic().run()
+                        }
+                        className='hover:bg-neutral-100 py-1'>
                         <ItalicIcon className='size-4 me-2' />
                         Italic
                         <MenubarShortcut className='ms-auto text-xs tracking-widest text-muted-foreground'>
                           ⌘ I
                         </MenubarShortcut>
                       </MenubarItem>
-                      <MenubarItem className='hover:bg-neutral-100 py-1'>
+                      <MenubarItem
+                        onClick={() =>
+                          editor?.chain().focus().toggleUnderline().run()
+                        }
+                        className='hover:bg-neutral-100 py-1'>
                         <UnderlineIcon className='size-4 me-2' />
                         Underline
                         <MenubarShortcut className='ms-auto text-xs tracking-widest text-muted-foreground'>
                           ⌘U
                         </MenubarShortcut>
                       </MenubarItem>
-                      <MenubarItem className='hover:bg-neutral-100 py-1'>
+                      <MenubarItem
+                        onClick={() =>
+                          editor?.chain().focus().toggleStrike().run()
+                        }
+                        className='hover:bg-neutral-100 py-1'>
                         <StrikethroughIcon className='size-4 me-2' />
                         Strikethrough
                         <MenubarShortcut className='ms-auto text-xs tracking-widest text-muted-foreground'>
@@ -222,7 +316,11 @@ const Navbar = () => {
                       </MenubarItem>
                     </MenubarSubContent>
                   </MenubarSub>
-                  <MenubarItem className='hover:bg-neutral-100 py-1'>
+                  <MenubarItem
+                    onClick={() =>
+                      editor?.chain().focus().unsetAllMarks().run()
+                    }
+                    className='hover:bg-neutral-100 py-1'>
                     <RemoveFormattingIcon className='size-4 me-2' />
                     Remove Formatting
                   </MenubarItem>
